35208ef4486c1ec5acc46bb93e483c4a7fdb300c
char502 <char502@hotmail.com>
2019-09-18 21:45:58 +0100

    testing map data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Slider from "react-slick";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import "./index.css";
+
+class App extends React.Component {
+  state = {
+    news: []
+  };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      "https://newsapi.org/v2/top-headlines?country=gb&apiKey=844f83db9ed44325a55725ad85a1592c"
+    );
+
+    console.log(response);
+    const news = response.data.articles;
+
+    this.setState({
+      news
+    });
+  }
+
+  render() {
+    console.log(this.state.news);
+
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 50000,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <div>
+        <h2>Single Item</h2>
+        {/* {this.state.news.map((article) => (
+          <div key={article.title}>{article.title}</div>
+        ))} */}
+        <Slider {...settings}>
+          {this.state.news.map((article) => (
+            <div key={article.title}>{article.title}</div>
+          ))}
+        </Slider>
+      </div>
+    );
+  }
 }
 
 export default App;
