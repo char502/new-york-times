cadee5da053da82bad1dfdafa2b5c427c61abc1d
char502 <char502@hotmail.com>
2019-09-30 17:09:24 +0100

    how to cancel an axios request?

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,6 +1,8 @@
 import React from "react";
 import queryString from "query-string";
 import { getSearchNews } from "../utils/api";
+// import axios from "axios";
+// import axiosCancel from "axios-cancel";
 
 class SearchResults extends React.Component {
   state = {
@@ -13,6 +15,20 @@ class SearchResults extends React.Component {
     const news = await getSearchNews(query.searchTerm, query.sources);
     console.log(news);
     this.setState({ loading: false, results: news.data.articles });
+
+    // try {
+    //   let query = queryString.parse(this.props.location.search);
+    //   this.setState({ loading: true });
+    //   const news = await getSearchNews(query.searchTerm, query.sources);
+    //   console.log(news);
+    //   this.setState({ loading: false, results: news.data.articles });
+    // } catch (thrown) {
+    //   if (axiosCancel.isCancel(thrown)) {
+    //     console.log("request cancelled");
+    //   } else {
+    //     console.log("some other reason");
+    //   }
+    // }
   };
 
   componentDidMount() {
@@ -25,6 +41,10 @@ class SearchResults extends React.Component {
       this.getData();
     }
   }
+
+  // componentWillUnmount() {
+  //   axiosCancel(getSearchNews());
+  // }
 
   render() {
     const { results } = this.state;
