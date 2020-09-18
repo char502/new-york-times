8d0b5a7beaa0357e6c03c445d058d82dfb97c9ab
Charlotte Ellwood <char502@hotmail.com>
2020-09-18 14:38:25 +0100

    working on adding an express backend

---
+++ backend/app.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const { getNews, getSearchNews } = require("./api");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  const { sources, search } = req.query;
+
+  if (search) {
+    getSearchNews(search, sources).then(({ data }) => {
+      return res.json({ data: data.articles });
+    });
+  } else {
+    getNews(req.query.sources).then(({ data }) => {
+      return res.json({ data: data.articles });
+    });
+  }
+});
+
+app.listen();
+
+// const express = require("express");
+// const fetch = require("node-fetch");
+// // let request = require("request");
+// var apiCalls = require("../src/utils/api");
+// const app = express();
+// const port = 5000;
+
+// app.get('/wf', wf.foo );
+
+// app.get("/apiCalls", apiCalls.getNews);
+// app.get("/apiCalls", apiCalls.getSearchNews);
+
+// app.get("/apiCalls", (req, res) => {
+//   console.log(`request object is ${req}, response object is ${res}`);
+// });
+
+// app.get("//newsapi/everything", (req, res) => {
+//   request(
+//     "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=The key here",
+//     function (error, response, body) {
+//       if (!error && response.statusCode === 200) {
+//         // console.log(body);
+//         // let parsedBody = JSON.parse(body);
+//         res.send(body);
+//       }
+//     }
+//   );
+// });
+
+// res.send("Hello World"));
+
+// app
+//   .route("/newsapi")
+//   .all((req, res, next) => {
+//     //Any auth method required
+//     next();
+//   })
+//   .get(async (req, res, next) => {
+//     const url = req.originalUrl.replace("/newsapi", "");
+//     return fetch(
+//       `${news_api_host}${url}&apiKey=${process.env.react_app_api_key}`
+//     );
+//   })
+//   .post(async (req, res, next) => {
+//     // whatever post API instructions you might have
+//   });
+
+// app.get("/", (req, res) => res.send("Hello World"));
+// app.get("//newsapi/everything", (req, res) => {
+//   request(
+//     "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=The key here",
+//     function (error, response, body) {
+//       if (!error && response.statusCode === 200) {
+//         // console.log(body);
+//         // let parsedBody = JSON.parse(body);
+//         res.send(body);
+//       }
+//     }
+//   );
+// });
+
+// res.send("Hello World"));
+
+// app.listen(port, () => console.log(`Example app listening on port ${port} `));
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,19 +1,51 @@
-import axios from "axios";
+const axios = require("axios");
+// import axios from "axios";
 
 const baseUrl = "https://newsapi.org/v2";
 
-export const getNews = (path) => {
+const getNews = (path) => {
   const link = `${baseUrl}/top-headlines?sources=${path}&apiKey=${process.env.react_app_api_key}`;
-
   return axios.get(link);
 };
 
-export const getSearchNews = (searchTerm, sources) => {
+const getSearchNews = (searchTerm, sources) => {
   let link = "";
   if (sources) {
     link = `${baseUrl}/everything?q=${searchTerm}&sources=${sources}&apiKey=${process.env.react_app_api_key}`;
   } else {
-    link = `${baseUrl}/everything?q=${searchTerm}&apiKey=${process.env.react_app_api_key}`;
+    link = `${baseUrl}/everything?q=${searchTerm}&apiKey=${process.env.API_KEY}`;
   }
   return axios.get(link);
 };
+
+module.exports = {
+  getNews,
+  getSearchNews,
+};
+
+// import axios from "axios";
+
+// const baseUrl = "https://newsapi.org/v2";
+
+// const headlines = "/top-headlines?sources";
+
+// export const getNews = (path) => {
+//   // const link = `apiCall/`
+
+//   const link = `${baseUrl}/top-headlines?sources=${path}&apiKey=${process.env.react_app_api_key}`;
+
+//   const apiCall = link;
+
+//   console.log(link);
+//   return axios.get(link);
+// };
+
+// export const getSearchNews = (searchTerm, sources) => {
+//   let link = "";
+//   if (sources) {
+//     link = `${baseUrl}/everything?q=${searchTerm}&sources=${sources}&apiKey=${process.env.react_app_api_key}`;
+//   } else {
+//     link = `${baseUrl}/everything?q=${searchTerm}&apiKey=${process.env.react_app_api_key}`;
+//   }
+//   return axios.get(link);
+// };
