1566eabb04195fe829c5a21a82bccc8012b5d4e0
char502 <char502@hotmail.com>
2020-10-13 12:34:19 +0100

    tidies up files

--- src/components/Carousel/CarouselButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { CarouselButton } from "../Button";
-
-export const CustomArrow = (props) => (
-  <CarouselButton small type="button" {...props}>
-    {props.next ? ">" : "<"}
-  </CarouselButton>
-);
