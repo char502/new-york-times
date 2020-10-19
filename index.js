3311c111b9bd764687b52e67705ccd310f17932d
char502 <char502@hotmail.com>
2020-10-19 16:07:40 +0100

    is this working now too

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 const port = process.env.PORT || 5000;
 
-const {getNews, getSearchNews} = require('./apiData')
+const {getNews, getSearchNews} = require('./api/apiData')
 
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,24 +30,16 @@ app.get('/newsDataApi', (req, res) => {
 
 })
 
-<<<<<<< HEAD:api/index.js
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   // Serve any static files
+//   app.use(express.static(path.join(__dirname, 'client/build')));
     
-  // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
+//   // Handle React routing, return all requests to React app
+//   app.get('*', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
 
-=======
-// app.use(express.static(path.resolve(__dirname, "client", "build")));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// }); 
->>>>>>> old-working-locally-state:index.js
 
 
 
