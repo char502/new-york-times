b8593831837edeaa7e0cce63e9e829120872aadd
char502 <char502@hotmail.com>
2020-10-20 13:33:19 +0100

    changed server.js

--- api/server.js
+++ api/server.js
@@ -41,11 +41,11 @@ app.get('/newsDataApi', (req, res) => {
 //   });
 // }
 
-// app.use(express.static(path.resolve(__dirname, "client", "build")));
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// }); 
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+}); 
 
 
 
